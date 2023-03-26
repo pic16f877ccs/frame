7dec689f221db3cf1834329ad5f7072623f125b0
Karl Wulfert <pic16f877.ccs@emailn.de>
2023-03-26 19:24:43 +0200

    Add color

--- src/main.rs
+++ src/main.rs
@@ -38,12 +38,19 @@ impl Frame {
         }
     }
 
+    fn torn() -> Self {
+        Self {
+            frame_variants: ['', '', '', '', '¦', '¦', '', ''],
+        }
+    }
+
     fn frame_variants(app: &ArgMatches) -> Self {
         if let Some(variants) = app.get_one::<String>("frame") {
             match variants.as_str() {
                 "duble" => Self::duble(),
                 "round" => Self::round(),
                 "heavy" => Self::heavy(),
+                "torn" => Self::torn(),
                 _ => Self::default(),
             }
         } else {
@@ -51,8 +58,8 @@ impl Frame {
         }
     }
     fn frame_custom(&mut self, app: &ArgMatches) -> &Self {
-        self.set_hor_upper(*app.get_one("hor-upper").unwrap_or(&self.get_hor_upper()));
-        self.set_hor_lower(*app.get_one("hor-lower").unwrap_or(&self.get_hor_lower()));
+        self.set_hor_upper(*app.get_one("hor-upper").unwrap_or(&self.get_hor_top()));
+        self.set_hor_lower(*app.get_one("hor-lower").unwrap_or(&self.get_hor_buttom()));
         self.set_vert_left(*app.get_one("vert-left").unwrap_or(&self.get_vert_left()));
         self.set_vert_right(*app.get_one("vert-right").unwrap_or(&self.get_vert_right()));
         self.set_top_left(*app.get_one("top-left").unwrap_or(&self.get_top_left()));
@@ -74,10 +81,10 @@ impl Frame {
     fn get_top_right(&self) -> char {
         self.frame_variants[1]
     }
-    fn get_hor_upper(&self) -> char {
+    fn get_hor_top(&self) -> char {
         self.frame_variants[2]
     }
-    fn get_hor_lower(&self) -> char {
+    fn get_hor_buttom(&self) -> char {
         self.frame_variants[3]
     }
     fn get_vert_left(&self) -> char {
@@ -132,9 +139,6 @@ fn main() -> Result<(), Box<dyn Error>> {
     let mut frame = Frame::frame_variants(&app);
     frame.frame_custom(&app);
     let mut buff = String::new();
-    let mut hor_upper_line = String::from(frame.get_hor_upper());
-    let mut hor_lower_line = String::from(frame.get_hor_lower());
-    let mut te = String::from("");
 
     match app.get_one::<PathBuf>("file") {
         Some(path) => buff = fs::read_to_string(path)?,
@@ -143,31 +147,52 @@ fn main() -> Result<(), Box<dyn Error>> {
         }
     }
 
-    let max_line_len: usize = match buff.lines().map(|line| line.chars().count()).max() {
-        Some(value) => value,
+    let expand = *app.get_one("expand").unwrap_or(&0);
+    let mut max_line_len: usize = match buff.lines().map(|line| line.chars().count()).max() {
+        Some(value) => value + expand * 2,
         None => return Ok(()),
     };
 
-    let expand = *app.get_one("expand").unwrap_or(&0);
+    let color = if let Some(color) = app.get_one::<String>("color") {
+        match color.as_str() {
+            "red" => "red",
+            "green" => "green",
+            "yellow" => "yellow",
+            "blue" => "blue",
+            "magenta" => "magenta",
+            "cyan" => "cyan",
+            "white" => "white",
+            _ => "black",
+        }
+    } else {
+        "black"
+    };
 
-    hor_upper_line = hor_upper_line.repeat(max_line_len + expand * 2);
-    hor_lower_line = hor_lower_line.repeat(max_line_len + expand * 2);
-    te = " ".repeat(max_line_len + expand * 2);
-    te = format!(
-        "{vrt_left}{te}{vrt_right}\n",
-        vrt_left = frame.get_vert_left(),
-        vrt_right = frame.get_vert_right()
+    let hor_top_line = String::from(frame.get_hor_top())
+        .repeat(max_line_len)
+        .color(color);
+    let hor_buttom_line = String::from(frame.get_hor_buttom())
+        .repeat(max_line_len)
+        .color(color);
+    let vrt_left = String::from(frame.get_vert_left()).color(color);
+    let vrt_right = String::from(frame.get_vert_right()).color(color);
+
+    let hor_line = format!(
+        "{vrt_left}{hor_line}{vrt_right}\n",
+        hor_line = " ".repeat(max_line_len)
     )
     .repeat(expand);
 
-    hor_upper_line = format!(
-        "{top_left}{hor_upper_line}{top_right}\n{te}{buff}",
-        top_left = frame.get_top_left(),
-        top_right = frame.get_top_right()
+    let buff_line = format!(
+        "{top_left}{hor_top_line}{top_right}\n{hor_line}{buff}{hor_line}{bottom_left}{hor_buttom_line}{bottom_right}\n",
+        top_left = frame.get_top_left().to_string().color(color),
+        top_right = frame.get_top_right().to_string().color(color),
+        bottom_left = frame.get_bottom_left().to_string().color(color),
+        bottom_right = frame.get_bottom_right().to_string().color(color),
     );
 
-    for current_line in hor_upper_line.lines() {
-        let current_line_len = current_line.chars().count() + expand * 2;
+    for current_line in buff_line.lines() {
+        let current_line_len = current_line.chars().count();
 
         let out_line = if max_line_len < current_line_len {
             format!("{current_line}\n")
@@ -182,26 +207,16 @@ fn main() -> Result<(), Box<dyn Error>> {
                     _ => left = 0,
                 }
             }
+
             format!(
-                "{vert_left}{left_fill}{current_line}{right_fill}{vert_right}\n",
-                vert_left = frame.get_vert_left(),
+                "{vrt_left}{left_fill}{current_line}{right_fill}{vrt_right}\n",
                 left_fill = " ".repeat(left),
                 right_fill = " ".repeat(line_len - left),
-                vert_right = frame.get_vert_right(),
             )
         };
 
         std::io::stdout().write_all(out_line.as_bytes())?;
     }
-
-    std::io::stdout().write_all(
-        format!(
-            "{te}{bottom_left}{hor_lower_line}{bottom_right}\n",
-            bottom_left = frame.get_bottom_left(),
-            bottom_right = frame.get_bottom_right(),
-        )
-        .as_bytes(),
-    )?;
 
     Ok(())
 }
@@ -218,7 +233,7 @@ fn app_commands() -> ArgMatches {
                 .num_args(1)
                 .value_name("VARIANTS")
                 .help("Text frame variants")
-                .value_parser(["duble", "round", "heavy"])
+                .value_parser(["duble", "round", "heavy", "torn"])
                 //.hide_possible_values(true)
                 .required(false),
         )
@@ -327,10 +342,7 @@ fn app_commands() -> ArgMatches {
                 .long("color")
                 .value_name("COLOR")
                 .help("Displays a text frame in the specified color")
-                .value_parser([
-                    "black", "red", "green", "yellow", "blue", "magenta", "cyan", "white",
-                ])
-                .default_value("black")
+                .value_parser(["red", "green", "yellow", "blue", "magenta", "cyan", "white"])
                 .hide_default_value(true)
                 .hide_possible_values(true)
                 .required(false),
